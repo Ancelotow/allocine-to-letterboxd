3667c5336abd429796a6a0430ca370709d29fd9b
Owen ANCELOT <owen.ancelot@la-projets.com>
2025-09-02 22:30:43 +0200

    .feat: implement review fetching and mapping from Allocine API

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"allocine-letterboxd-sync-reviews/allocine"
 
@@ -14,6 +15,37 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	allocine.GetReviews()
+	opinions := allocine.GetReviews()
+	reviews := make([]Review, len(opinions))
+	for i, opinion := range opinions {
+		reviews[i] = mapToReview(opinion)
+	}
 
+	log.Printf("Fetched %d reviews from Allocine", len(reviews))
+}
+
+func mapToReview(opinion allocine.Node) Review {
+	layout := "2006-01-02T15:04:05"
+	createdAt, err := time.Parse(layout, opinion.Node.Opinion.CreatedAt)
+	if err != nil {
+		log.Fatal("Error parsing date:", err)
+	}
+
+	return Review{
+		AllocineId: opinion.Node.Entity.InternalID,
+		MovieTitle: opinion.Node.Entity.Title,
+		MovieYear:  opinion.Node.Entity.Data.ProductionYear,
+		ReviewAt:   createdAt,
+		Rating:     opinion.Node.Opinion.Content.Rating,
+		Review:     opinion.Node.Opinion.Content.Review,
+	}
+}
+
+type Review struct {
+	AllocineId int
+	MovieTitle string
+	MovieYear  int
+	ReviewAt   time.Time
+	Rating     float32
+	Review     string
 }
